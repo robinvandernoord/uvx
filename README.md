8358d2a5b7733cfda4a5c655eec0c15d28221bb2
Robin van der Noord <robinvandernoord@gmail.com>
2024-02-29 16:20:52 +0100

    feat: empty hatch project

---
+++ README.md
@@ -0,0 +1,5 @@
+# uvx: pipx for uv
+
+Inspired by:
+- [pipx](https://github.com/pypa/pipx)
+- [uv](https://github.com/astral-sh/uv)
---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2024-present Robin van der Noord <robinvandernoord@gmail.com>
+#
+# SPDX-License-Identifier: MIT
