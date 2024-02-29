293839ff11bee2b77c0bae2d22c1e442d235d3b9
Robin van der Noord <robinvandernoord@gmail.com>
2024-02-29 16:26:03 +0100

    chore: more  basic setup

--- src/uvx/__about__.py
+++ src/uvx/__about__.py
@@ -1,4 +1,6 @@
+"""This file stores the package version."""
+
 # SPDX-FileCopyrightText: 2024-present Robin van der Noord <robinvandernoord@gmail.com>
 #
 # SPDX-License-Identifier: MIT
-__version__ = "0.0.1"
+__version__ = "0.0.0"
--- src/uvx/__init__.py
+++ src/uvx/__init__.py
@@ -1,3 +1,5 @@
+"""This file exposes the main package functions."""
+
 # SPDX-FileCopyrightText: 2024-present Robin van der Noord <robinvandernoord@gmail.com>
 #
 # SPDX-License-Identifier: MIT
